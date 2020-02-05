9e7135285751e62181e1f1bfc74ea9ca57365859
jpdalamani@gmail.com <jpdalamani@gmail.com>
2020-02-05 01:09:25 -0800

    Added HumanBody class and moved Sex attribute from Body to HumanBody

--- Body.cs
+++ Body.cs
@@ -12,10 +12,7 @@ namespace LiftTool
 
         private int weight;
 
-        private bool sex;
-
         protected int Age { get => age; set => age = value; }
         protected int Weight { get => weight; set => weight = value; }
-        protected bool Sex { get => sex; set => sex = value; }
     }
 }
---
+++ HumanBody.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftTool
+{
+    class HumanBody : Body
+    {
+        private bool sex;
+
+        public bool Sex { get => sex; set => sex = value; }
+    }
+}
