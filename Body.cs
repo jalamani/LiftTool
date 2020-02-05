f1b9d473615bc16a9a32c3be7caa0b45350464f2
jpdalamani@gmail.com <jpdalamani@gmail.com>
2020-02-05 00:49:59 -0800

    Added Body abstract class to Project

---
+++ Body.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftTool
+{
+    public abstract class Body
+    {
+        private int age;
+
+        private int weight;
+
+        private bool sex;
+
+        protected int Age { get => age; set => age = value; }
+        protected int Weight { get => weight; set => weight = value; }
+        protected bool Sex { get => sex; set => sex = value; }
+    }
+}
