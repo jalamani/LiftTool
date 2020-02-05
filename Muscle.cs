7d62352399cc8f951373393d119f3774641fbefe
jpdalamani@gmail.com <jpdalamani@gmail.com>
2020-02-05 01:14:13 -0800

    Added Muscle class

---
+++ Muscle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftTool
+{
+    class Muscle
+    {
+        private int strength;
+
+        public int Strength { get => strength; set => strength = value; }
+    }
+}
